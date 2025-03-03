9e05be858c8bcc89e9b55ba53106c967435024b7
geethasamynathan <geethasamynathan2011@gmail.com>
2025-03-03 08:12:40 +0530

    test added

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -30,6 +30,12 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
+        public IActionResult CICDPipeline()
+        {
+            return View();
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
